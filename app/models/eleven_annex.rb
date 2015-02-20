bd5b73f403ae284bc2a16b2019ff0d525ae35cae
fruizg0302 <fruizg0302@icloud.com>
2015-02-20 12:38:25 -0600

    Cambios al anexo once y upgrading version

--- app/models/eleven_annex.rb
+++ app/models/eleven_annex.rb
@@ -1,6 +1,6 @@
 class ElevenAnnex < ActiveRecord::Base
   def self.state_results(state_id)
-    self.find_by_sql("select  na.id, na.numero_frascos, na.numero_dosis, na.placas, na.chinchos, na.comprobantes_cartillas, na.agujas, na.jerfingas, na.thermos ,na.jurisdiction_id from ten_annexes as na inner join jurisdictions as j on j.id = na.jurisdiction_id inner join states as st on j.state_id = st.id where st.id =#{state_id} ")
+    self.find_by_sql("select  na.id, na.numero_frascos, na.numero_dosis, na.placas, na.chinchos, na.comprobantes_cartillas, na.agujas, na.jeringas, na.thermos ,na.jurisdiction_id from eleven_annexes as na inner join jurisdictions as j on j.id = na.jurisdiction_id inner join states as st on j.state_id = st.id where st.id =#{state_id} ")
   end
 
   def self.captured_jurisdictions(state_id)
